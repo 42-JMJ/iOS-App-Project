6ae934088441fcacd0e04f629a0c4d9b07c0b57f
Mihye Kim <mihykim@c4r1s7.42seoul.kr>
2020-04-27 19:45:13 +0900

    오늘날짜표시, 키보드팝업, 텍스트뷰힌트 추가

--- OneDayOneAnswer/OneDayOneAnswer/TodayViewController.swift
+++ OneDayOneAnswer/OneDayOneAnswer/TodayViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class TodayViewController: UIViewController {
-
+class TodayViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet var labelDate: UILabel!
+    @IBOutlet var textViewAnswer: UITextView!
+    @IBOutlet var labelPlaceHolder: UILabel!
+    
+    func DateAsString() -> String {
+        let today = Date() // 현재시간을 가져옴
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 dd일"
+        let dateString = formatter.string(from: today)
+        return dateString
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        labelDate.textAlignment = .center
+        labelDate.text = String(DateAsString())
+        
+        textViewAnswer.delegate = self
+        textViewAnswer.layer.borderWidth = 1.0
+        textViewAnswer.layer.borderColor = UIColor.darkGray.cgColor
+        textViewAnswer.layer.cornerRadius = 10 // 모서리 둥글게
+        textViewAnswer.textContainerInset = UIEdgeInsets(top: 20, left: 5, bottom: 20, right: 5)
     }
-
-
+    
+    func textViewDidChange(_ textViewAnswer: UITextView) {
+        labelPlaceHolder.isHidden = !textViewAnswer.text.isEmpty
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
