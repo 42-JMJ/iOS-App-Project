ea2992528d8d6d6bd77b3e9d0202e4c314bdb94e
Mihye Kim <mihykim@c4r1s7.42seoul.kr>
2020-04-28 12:42:58 +0900

    setDate 수정해서 viewDidLoad 간결화

--- OneDayOneAnswer/OneDayOneAnswer/TodayViewController.swift
+++ OneDayOneAnswer/OneDayOneAnswer/TodayViewController.swift
@@ -10,38 +10,36 @@ import UIKit
 
 class TodayViewController: UIViewController, UITextViewDelegate {
     
-    @IBOutlet var labelDate: UILabel!
-    @IBOutlet var textViewAnswer: UITextView!
+    @IBOutlet var labelDate:        UILabel!
+    @IBOutlet var textViewAnswer:   UITextView!
     @IBOutlet var labelPlaceHolder: UILabel!
+    @IBOutlet var btnSave:          UIButton!
     
-    func DateAsString() -> String {
-        let today = Date() // 현재시간을 가져옴
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy년 MM월 dd일"
-        let dateString = formatter.string(from: today)
-        return dateString
+    func setDate() {
+        let today               =   Date()
+        let formatter           =   DateFormatter()
+        formatter.dateFormat    =   "yyyy년 MM월 dd일"
+        labelDate.textAlignment =   .center
+        labelDate.text          =   String(formatter.string(from: today))
     }
     
     func setTextView() {
-        self.textViewAnswer.layer.borderWidth = 1.0
-        self.textViewAnswer.layer.borderColor = UIColor.darkGray.cgColor
-        self.textViewAnswer.layer.cornerRadius = 10 // 모서리 둥글게
-        self.textViewAnswer.textContainerInset = UIEdgeInsets(top: 20, left: 5, bottom: 20, right: 5)
+        self.textViewAnswer.layer.borderWidth   =   1.0
+        self.textViewAnswer.layer.borderColor   =   UIColor.darkGray.cgColor
+        self.textViewAnswer.layer.cornerRadius  =   10
+        self.textViewAnswer.textContainerInset
+            = UIEdgeInsets(top: 20, left: 5, bottom: 20, right: 5)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        labelDate.textAlignment = .center
-        labelDate.text = String(DateAsString())
-        
-        textViewAnswer.delegate = self
+        textViewAnswer.delegate =   self
+        setDate()
         setTextView()
     }
     
     func textViewDidChange(_ textViewAnswer: UITextView) {
-        labelPlaceHolder.isHidden = !textViewAnswer.text.isEmpty
+        labelPlaceHolder.isHidden           =   !textViewAnswer.text.isEmpty
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
