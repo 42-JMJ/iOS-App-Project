db315b8391107b294d6133958e98c6ef33baa615
Mac <MacBookPro@Macui-MacBook-Pro.local>
2020-04-29 11:50:40 +0900

    리스트 스토리보드 구현

--- OneDayOneAnswer/OneDayOneAnswer/ListViewController.swift
+++ OneDayOneAnswer/OneDayOneAnswer/ListViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import Foundation
-
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
     
 }
