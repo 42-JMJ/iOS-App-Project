8ebf6175fe0f8b0928888129244aac93942601aa
Mac <MacBookPro@Macui-MacBook-Pro.local>
2020-05-04 19:05:51 +0900

    리스트 db연동

--- OneDayOneAnswer/OneDayOneAnswer/ListViewController.swift
+++ OneDayOneAnswer/OneDayOneAnswer/ListViewController.swift
@@ -12,6 +12,8 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     @IBOutlet weak var tableView: UITableView!
     
+    public var sqldb: DataBase = SqliteDataBase.instance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,27 +22,50 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 31
+        let article = sqldb.selectArticles()
+        
+        return article.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        let article = sqldb.selectArticles()
+        let item = article[indexPath.row]
         
-        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        cell.labelQuestion?.text = item.question
+        cell.labelAnswer?.text = item.answer
+        cell.labelDate?.text = dateToStr(item.date, "M월 d일")
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let article = sqldb.selectArticles()
+        let item = article[indexPath.row]
+        let today = Date()
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
-        
-        if cell.labelAnswer.text == "" {
-            return 140
+        if item.date > today {
+            return 0
         } else {
             return 200
         }
     }
     
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let nextViewController: TodayViewController = segue.destination as? TodayViewController else{
+            return
+        }
+        guard let cell: UITableViewCell = sender as? UITableViewCell else {
+            return
+        }
+        
+        let indexPath = tableView.indexPath(for: cell)
+        let article = sqldb.selectArticles()
+        let item = article[(indexPath?.row)!]
+        
+        nextViewController.textToSet = item.date
+    }
+    */
 }
--- OneDayOneAnswer/OneDayOneAnswer/TableViewCell.swift
+++ OneDayOneAnswer/OneDayOneAnswer/TableViewCell.swift
@@ -15,25 +15,11 @@ class TableViewCell: UITableViewCell {
     @IBOutlet weak var labelQuestion: UILabel!
     @IBOutlet weak var labelAnswer: UILabel!
     
-    func setDateAndArticle(date: Date?) {
-        let today: Date
-        if date == nil {
-            today = Date()
-        } else {
-            today = date!
-        }
-        let formatter           =   DateFormatter()
-        formatter.dateFormat    =   "MM월 dd일"
-        labelDate.textAlignment =   .center
-        labelDate.text          =   String(formatter.string(from: today))
-        
-        let db: DataBase = TestDataBase.instance
-        labelQuestion.text = db.selectArticle(date: today)?.question
-        labelAnswer.text = db.selectArticle(date: today)?.answer
-    }
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        setDateAndArticle(date: nil)
+        cardView.layer.borderColor = UIColor.black.cgColor
+        cardView.layer.borderWidth = 0.55
+        cardView.layer.cornerRadius = 1
     }
 }
