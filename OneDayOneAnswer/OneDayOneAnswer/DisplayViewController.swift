077c8afb97e229a391ff8a7cd90e1d384361360a
365kim <365listener@gmail.com>
2020-05-05 18:14:48 +0900

    btnQuestion 그림자 효과 추가

--- OneDayOneAnswer/OneDayOneAnswer/DisplayViewController.swift
+++ OneDayOneAnswer/OneDayOneAnswer/DisplayViewController.swift
@@ -12,10 +12,18 @@ import UIKit
 
 class DisplayViewController: UIViewController {
     
+    @IBOutlet var btnQuestion: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setBtnQuestionStyle()
     }
-    
-    
+    func setBtnQuestionStyle() {
+        btnQuestion.layer.shadowColor = UIColor.black.cgColor
+        btnQuestion.layer.shadowOffset = CGSize(width: 10, height: 10)
+        btnQuestion.layer.shadowOpacity = 0.4
+        btnQuestion.layer.shadowRadius = 10
+        btnQuestion.layer.masksToBounds = false
+    }
 }
