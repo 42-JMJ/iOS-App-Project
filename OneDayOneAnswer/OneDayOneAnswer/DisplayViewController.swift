c0af698f9e2ed94a4af8d7acd16f344cd9a216c1
365kim <365listener@gmail.com>
2020-05-06 18:37:00 +0900

    scrollView 구현, boxBottom 높이 답변길이에 맞게 자동조정

--- OneDayOneAnswer/OneDayOneAnswer/DisplayViewController.swift
+++ OneDayOneAnswer/OneDayOneAnswer/DisplayViewController.swift
@@ -18,21 +18,23 @@ class DisplayViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var labelQuestion: UILabel!
     @IBOutlet var labelAnswer: UILabel!
-    @IBOutlet var whiteBoxTop: UILabel!
-    @IBOutlet var whiteBoxBottom: UILabel!
+    
+    @IBOutlet var boxTop: UILabel!
+    @IBOutlet var boxBottom: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setLayerRank()
         setComponentsBackground()
+        adjustBottomBoxHeight()
     }
     
     func setComponentsBackground() {
-        whiteBoxTop.layer.cornerRadius = 15
-        whiteBoxTop.layer.masksToBounds = true
-        whiteBoxBottom.layer.cornerRadius = 15
-        whiteBoxBottom.layer.masksToBounds = true
+        boxTop.layer.cornerRadius = 15
+        boxTop.layer.masksToBounds = true
+        boxBottom.layer.cornerRadius = 15
+        boxBottom.layer.masksToBounds = true
     }
     
     func setLayerRank() {
@@ -41,10 +43,17 @@ class DisplayViewController: UIViewController {
         btnEdit.layer.zPosition = 2
         labelQuestion.layer.zPosition = 2
         labelAnswer.layer.zPosition = 2
-        whiteBoxTop.layer.zPosition = 1
-        whiteBoxBottom.layer.zPosition = 1
+        boxTop.layer.zPosition = 1
+        boxBottom.layer.zPosition = 1
     }
     
+    func adjustBottomBoxHeight() {
+        labelAnswer.numberOfLines = 0
+        labelAnswer.textColor = .white
+        labelAnswer.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda. \n Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+        labelAnswer.sizeToFit()
+        boxBottom.frame.size.height = labelAnswer.frame.size.height + 140
+    }
 /*
     let strokeTextAttr = [
         NSAttributedString.Key.strokeColor : UIColor.white,
