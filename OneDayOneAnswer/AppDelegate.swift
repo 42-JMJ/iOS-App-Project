ee8f7e46f079b7f5085bcffabeedb223820544c9
nadarm <ccc7497@gmail.com>
2020-05-28 17:49:26 +0900

    add firebase(pod) and change directories

--- OneDayOneAnswer/AppDelegate.swift
+++ OneDayOneAnswer/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        sleep(3)
+        FirebaseApp.configure()
+        sleep(1)
         return true
     }
 
